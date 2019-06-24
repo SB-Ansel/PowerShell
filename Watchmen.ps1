cb7c3c4b86f10d552e56e88bf10c60ae1f33384f
Ansel <jake.madden@hotmail.co.uk>
2019-06-24 21:24:32 +0100

    Added Watchmen
    Watchmen, indefinitely loops and opens a program if it's closed until the parent PS window is closed. Useful if you want a program like a remote desktop client like TeamViewer to always stay online.

---
+++ Watchmen.ps1
@@ -0,0 +1,33 @@
+# Watchmen restart script by SB-Ansel
+# Little script which indefinitely loops and opens a program if it's closed until the parent PS window is closed. Useful if you want a program like a remote desktop client like TeamViewer to always stay online.
+
+$Color = $host.ui.RawUI.ForegroundColor
+
+function IsItAlive(){
+    $ProcessName = Get-Process | Select-String "AnyDesk"
+    if ($ProcessName -match "AnyDesk"){
+        $host.ui.RawUI.ForegroundColor = "DarkCyan"
+        Write-Host "    AnyDesk already running. No further action required!"
+        $host.ui.RawUI.ForegroundColor = $Color
+    } elseif ($ProcessName -ne "AnyDesk") {
+        Start-Process -FilePath 'FilePath' # Need manually enter file path to executable
+        $host.ui.RawUI.ForegroundColor = "DarkRed"
+        Write-Host '    AnyDesk not running. Automatic restart initiated!'
+        $host.ui.RawUI.ForegroundColor = $Color
+    }else {
+        $host.ui.RawUI.ForegroundColor = "DarkRed"
+        Write-Host "    An unanticipated error has occurred contact Administator!"
+        $host.ui.RawUI.ForegroundColor = $Color
+    }
+}
+
+while ($true) {
+    Start-Sleep -Seconds 3600 # Time == 1 Hour
+    # Start-Sleep -Seconds 10 # Time == 10 Seconds Testing purpose
+    $i++
+    $Time = Get-Date -Format G
+    $host.ui.RawUI.ForegroundColor = "Yellow"
+    Write-Host 'Watchmen :> Script last run at :'$Time
+    $host.ui.RawUI.ForegroundColor = $Color
+    IsItAlive # Query Get-Process
+}
